5bc58a15d8a1adcdb1b30476ff8e98061b3deef9
suerteines <suerteines@ewha.ac.kr>
2024-07-25 03:11:53 +0900

    Title: 369 게임, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240725/369 게임/369-game.py
@@ -0,0 +1,12 @@
+n = int(input())
+i = 1
+
+for i in range (1, n+1):
+    if i % 3 == 0:
+        print("0", end=' ')
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print("0", end=' ')
+    elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
+        print("0", end=' ')
+    else:
+        print(i, end=' ')
