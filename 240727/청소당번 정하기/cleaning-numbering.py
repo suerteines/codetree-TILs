bddd602e72569e9ea7fee969d27fb51245d92c34
suerteines <suerteines@ewha.ac.kr>
2024-07-27 00:50:01 +0900

    Title: 청소당번 정하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240727/청소당번 정하기/cleaning-numbering.py
+++ 240727/청소당번 정하기/cleaning-numbering.py
@@ -4,9 +4,7 @@ cnt_1 = 0
 cnt_2 = 0
 cnt_3 = 0
 for i in range (n):
-    if i == 0:
-        pass
-    elif i % 12 == 0:
+    if i % 12 == 0:
         cnt_3 += 1
     elif i % 3 == 0:
         cnt_2 += 1
